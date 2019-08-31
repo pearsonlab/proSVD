da3e84385cf92832773d3f8db9f74620bd2c2e32
pgupta29 <pranjal.gupta@duke.edu>
2019-08-31 12:48:17 -0400

    added decay to streamingSVD, new notebook for AR toy testing

--- streamingSvd.py
+++ streamingSvd.py
@@ -3,7 +3,7 @@
 import numpy as np
 import scipy
 
-def getSvd(A, k, l1, l, num_iter):
+def getSvd(A, k, l1, l, num_iter, decay_alpha=1, silent=True):
     s = A.shape[1]
     A_init = A[:, 0:l1]
 
@@ -26,7 +26,7 @@ def getSvd(A, k, l1, l, num_iter):
         else:
             A_plus = A[:, t:t+l]
             t = t+l
-
+        
         #QR decomposition of additional data
         Q_T = np.transpose(Q)
         R_T = np.transpose(R)
@@ -43,9 +43,10 @@ def getSvd(A, k, l1, l, num_iter):
         tmp = np.append(tmp, R_perp, axis=1)
 
         R_hat = np.append(R_prev, tmp, axis=0)
-
+        
         #SVD of R_hat (B_hat)
         U, diag, V_T = np.linalg.svd(R_hat, full_matrices=False)
+        diag = np.power(diag, decay_alpha)
         V = np.transpose(V_T)
         
         #Orthogonal Procrustes singular basis
@@ -58,14 +59,13 @@ def getSvd(A, k, l1, l, num_iter):
         
         #Find T as product of U_tilda, V_tilda
         T = U_tilda.dot(V_tilda_T)
-        
         #Calculate new Q of this iteration using T
         #Q = Q_hat * U1 * T_transpose
         T_trans = np.transpose(T)
         G1 = U1.dot(T_trans)
         G1_T = np.transpose(G1)
         Q = Q_hat.dot(G1)
-
+        
         #Calculation of new R does not need Orthogonal Procrustes since
         #we do not care
         V1 = V[:,0:k]
@@ -78,7 +78,6 @@ def getSvd(A, k, l1, l, num_iter):
         #But G1_T = T * U1_T => R = T * U1_T * U1 * diag(S) * Tv_T
         #R = T * diag(S) * Tv_T
         R = T.dot(np.diag(diag[0:k]).dot(Tv_T))
-
         ##Orthogonal Procrustes singular basis
         #M = R_T.dot(G1_T.dot(R_hat))
         #V1 = V[:, 0:k]
@@ -95,9 +94,25 @@ def getSvd(A, k, l1, l, num_iter):
         #T_trans = np.transpose(T)
         #Gv1 = V1.dot(T_trans)
         #R = G1_T.dot(R_hat.dot(Gv1))
-
+        
+        if not silent:
+            print('Q_perp shape:\t' + str(Q_perp.shape))
+            print('R_perp shape:\t' + str(R_perp.shape))
+            print('Aplus shape:\t' + str(A_plus.shape))
+            print('R_hat shape:\t' + str(R_hat.shape))
+            print('U (Rhat) shape:\t' + str(U.shape))
+            print('Q shape :\t' + str(Q.shape))
+            print('Qhat shape :\t' + str(Q_hat.shape))
+            print('U1 shape:\t' + str(U1.shape))
+            print('M shape :\t' + str(M.shape))
+            print('T shape :\t' + str(T.shape))
+            print('G1 shape:\t' + str(G1.shape))
+            print('new Q shape:\t' + str(Q.shape))
+            print('R shape:\t' + str(R.shape))
+            
+            
     U, S, V = np.linalg.svd(R, full_matrices=False)
-    Q = Q.dot(U)
+    Qtrue = Q.dot(U)
 
-    return Q
+    return Qtrue, Q
 
