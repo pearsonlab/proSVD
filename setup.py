cd36a610a510569f2d4aed21e4e75cf474cf37db
pgupta29 <pranjal.gupta@duke.edu>
2022-07-22 18:38:00 -0400

    adding jupyter to requirements, cleaning up setup

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
 	long_description = fh.read()
 
 setuptools.setup(
-	name="proSVD: A streaming dimension reduction method",
+	name="proSVD",
 	version='0.0.1',
 	author="Pranjal Gupta, Anne Draelos",
 	author_email="pranjal.gupta@duke.edu",
 	description="Streaming dimension reduction tools for neural data",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
-	url="https://github.com/pearsonlab/streamingSVD",
+	url="https://github.com/pearsonlab/proSVD",
 	packages=setuptools.find_packages(),
 
 )
