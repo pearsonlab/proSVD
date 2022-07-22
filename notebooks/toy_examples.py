03a78ce29f0a9e6f82802714a3a4591f72b8dfa3
pgupta29 <pranjal.gupta@duke.edu>
2022-07-22 18:39:36 -0400

    adding options for other updates

--- notebooks/toy_examples.py
+++ notebooks/toy_examples.py
@@ -6,48 +6,48 @@ from matplotlib import cm; cmap = cm.Dark2
 from scipy.ndimage import gaussian_filter1d
 
 from proSVD.proSVD import proSVD
-import proSVD.utils as utils
+# from proSVD.utils import get_streamingSVD
 
 #%% switching between different spectral regimes
 np.random.seed(100)
 n = 6
 embed_dim = 60 # "neurons"
-t_latent = [200]  # time
-regime_changes = 15
+t_latent = [150]  # time
+regime_changes = 5
 
+# define spectral regimes
+Ss_true = np.array([[1, 1, 0, 0, 1, 0],
+                    [0, 0, 1, 1, 0, 1]])
+
+# generate data - keep U and V the same throughout
+U_true, _ = np.linalg.qr(np.random.normal(size=(n,n)))
+V_true, _ = np.linalg.qr(np.random.normal(size=(t_latent[0],t_latent[0])))
+V_true = gaussian_filter1d(V_true[:n, :], sigma=2)
+num_regimes = Ss_true.shape[0]
+noise = 0 #np.random.normal(scale=.05, size=(n,curr_t))
+regimes = [[] for i in range(num_regimes)]
+for i in range(num_regimes):
+    S_true = Ss_true[i, :]
+    regimes[i] = (U_true @ np.diag(S_true) @ V_true) + noise
+
+# picking which regimes go where
+# to switch every regime in order
+# regime_order = np.arange(regime_changes) % num_regimes
+# completely random
+regime_order = np.random.randint(num_regimes, size=regime_changes)
+# to sample a regime without replacement, then do it again once all regimes are sampled once
+# regime_order = np.array([np.random.choice(num_regimes, num_regimes, replace=False) for i in range(regime_changes)]).flatten()
+# regime_order = regime_order[:regime_changes+1]
+        
+currmode = 'pro'
 fig, ax = plt.subplots(len(t_latent), 3, figsize=(20,4*len(t_latent)),
             gridspec_kw={'hspace': 0.5, 'wspace': 0.3})
 if len(t_latent) == 1:
     ax = ax[np.newaxis, :]  # for plotting 1 row with ease
 
 for row, curr_t in enumerate(t_latent):
-    # generate data - keep U and V the same throughout
-    U_true, _ = np.linalg.qr(np.random.normal(size=(n,n)))
-    V_true, _ = np.linalg.qr(np.random.normal(size=(curr_t,curr_t)))
-    V_true = gaussian_filter1d(V_true[:n, :], sigma=2)
-
-    # define spectral regimes
-    Ss_true = np.array([[1, 1, 0, 0, 1, 0],
-                        [0, 0, 1, 1, 0, 1]])
-
-    num_regimes = Ss_true.shape[0]
-    noise = np.random.normal(scale=.05, size=(n,curr_t))
-    regimes = [[] for i in range(num_regimes)]
-    for i in range(num_regimes):
-        S_true = Ss_true[i, :]
-        regimes[i] = (U_true @ np.diag(S_true) @ V_true) + noise
-
     # append particular regime to true data
     latent_true = np.copy(regimes[-1])
-
-    # picking which regimes go where
-    # to switch every regime in order
-    regime_order = np.arange(regime_changes) % num_regimes
-    # completely random
-    # regime_order = np.random.randint(num_regimes, size=regime_changes)
-    # to sample a regime without replacement, then do it again once all regimes are sampled once
-    # regime_order = np.array([np.random.choice(num_regimes, num_regimes, replace=False) for i in range(regime_changes)]).flatten()
-    # regime_order = regime_order[:regime_changes+1]
 
     for curr_regime in regime_order:
         latent_true = np.append(latent_true, regimes[curr_regime], axis=1)
@@ -60,18 +60,19 @@ for row, curr_t in enumerate(t_latent):
     xs = embed_xs
 
     # PROTOTYPE FOR DOING ANALYSIS ON ANY DATASET
-    k = n
+    k = 6
     l1 = xs.shape[0]
     l = 1
     decay = 1
-    num_iters = np.ceil((xs.shape[1] - l1) / l).astype('int')
-    update_times = np.arange(1, num_iters) * l # index of when updates happen
+    num_iters = np.floor((xs.shape[1] - l1 - l)/l).astype('int')
+    print(num_iters)
+    update_times = np.arange(l1, num_iters*l, l) # index of when updates happen (not including init)
 
     # window for explained vars (TODO: also use this for keeping window of history/projs)
-    window = int(curr_t / 1)
+    window = int(curr_t / 1.5)
 
     # create object
-    pro = proSVD(k, w_len=l, w_shift=None, decay_alpha=decay, history=num_iters, trueSVD=True)
+    pro = proSVD(k, w_len=l, w_shift=None, decay_alpha=decay, history=num_iters, trueSVD=True, mode=currmode)
     A_init = xs[:, :l1]
     # init strategies:
     # svd
@@ -85,12 +86,52 @@ for row, curr_t in enumerate(t_latent):
     # pro.initialize(A_init, Q_init=Q_init, B_init=B_init)
     pro.initialize(A_init) # regular init
     # run it
-    projs, frac_vars, derivs = pro.run(xs, l1)
+    # projs, frac_vars, derivs, proj_mats = pro.run(xs, l1)   
+    # for svd and prosvd projections, variance explained
+    projs = [np.zeros((pro.k, xs.shape[1]-l1)) for i in range(2)]  # subtract l1 - init proj
+    frac_vars = [np.zeros(projs[i].shape) for i in range(2)]
+    # derivatives
+    derivs = np.zeros((pro.k, num_iters))
+    # comparing projection matrices over time - just norm(Q_prev@Q_prev.T - Q@Q.T))
+    proj_mats = np.zeros((num_iters))
+
+    # UUs, Ss = get_streamingSVD(xs, k, l1, l, num_iters, window=False)
+    # run proSVD online
+    for i, t in enumerate(update_times): 
+        start, end = t-l1, t+pro.w_len-l1
+        dat = xs[:, start+l1:end+l1]
+
+        # TODO: run should take user input pre/postupdate functions
+        # they should be executed here
+        pro.preupdate()
+        pro._updateSVD(dat)
+        pro.postupdate()
+
+        # TODO: move this to postupdate
+        # getting proj and variance explained
+        for j, basis in enumerate([pro.U, pro.Q]):
+            projs[j][:, start:end] = basis.T @ dat
+            # var explained over window
+            if window > 0 and end-window > 0: # process more data than window before getting var
+                curr_proj_vars = projs[j][:, end-window:end].var(axis=1)[:, np.newaxis]
+                total_vars = xs[:, end-window+l1:end+l1].var(axis=1)
+                frac_vars[j][:, start:end] = curr_proj_vars / total_vars.sum()
+            # else:  # cumulative variance
+            #     curr_proj_vars = projs[j][:, :end].var(axis=1)[:, np.newaxis]
+            #     total_vars = xs[:, l1:end].var(axis=1)
+            #     frac_vars[j][:, start:end] = curr_proj_vars / total_vars.sum()
+        # proSVD basis derivatives
+        derivs[:, i] = np.linalg.norm(pro.Q-pro.Q_prev, axis=0)
+        # proj mat changes
+        basis1 = pro.Us[:, :, i]
+        basis2 = pro.Us[:, :, i+1]
+        proj_mats[i] = np.linalg.norm((basis1 @ basis1.T) - (basis2 @ basis2.T))
+
     Qts, Scoll, Qcoll = (pro.Us, pro.Ss, pro.Qs)
 
 
-    # QcollU = np.zeros((xs.shape[0], k, num_iters))
-    # ScollU = np.zeros((xs.shape[0], num_iters))
+    # Us = np.zeros((xs.shape[0], k, num_iters))
+    # Ss = np.zeros((xs.shape[0], num_iters))
     # projsU = np.zeros((k, xs.shape[1]-l1))
     # frac_varsU = np.zeros(projsU.shape)
     # t = 0
@@ -98,8 +139,8 @@ for row, curr_t in enumerate(t_latent):
     #     U, S, V_T = np.linalg.svd(xs[:, :t+l1+l], full_matrices=False)
     #     t = t + l
     #     currU = U[:, :k]
-    #     QcollU[:, :, i-l1] = currU
-    #     ScollU[:, i-l1] = S
+    #     Us[:, :, i-l1] = currU
+    #     Ss[:, i-l1] = S
 
     #     projsU[:, i-l1:i+1-l1] = basis.T @ dat
     #     curr_proj_vars = projsU[:, :i].var(axis=1)[:, np.newaxis]
@@ -132,7 +173,7 @@ for row, curr_t in enumerate(t_latent):
     axtwin.set(ylabel='variance explained')
 
     titles = ['variance explained by \n left singular vectors', 
-              'variance explained by \n proSVD basis vectors']
+            'variance explained by \n proSVD basis vectors']
     for j in range(2):
         currax = ax[row, j+1]
         # individual and total var
@@ -140,33 +181,35 @@ for row, curr_t in enumerate(t_latent):
         # currax.plot(frac_vars[j][:, :t].sum(axis=0), ls='solid', color='k')
 
         # sum of vars for particular dimensions
-        dims_to_sum = int(n / num_regimes) # assuming first regime is in first n / num_regimes dims
-        labels = ['sum 1,2 dims', 'sum 3,4 dims', 'sum 5,6 dims']
+        dims_to_sum = int(k / num_regimes) # assuming first regime is in first n / num_regimes dims
+        labels = ['sum dims 1,2,3', 'sum dims 4,5,6']
         start = 0
         for p in range(num_regimes): 
             currax.plot(frac_vars[j][start:start+dims_to_sum, :t].sum(axis=0), lw=1.5,
                         ls='solid', color=cmap(p), label=labels[p])
+            currax.plot(frac_vars[j][:, :t].sum(axis=0), color='k', ls='--')
+            currax.set(ylim=(-.08, 1.1))
             start += dims_to_sum
         
         # labels
         currax.set_title(titles[j], y=1.08)
         currax.set(xlabel='bins seen', #'chunks seen (chunk size={})'.format(l), 
-                   ylabel='fraction of windowed variance explained')
+                ylabel='fraction of windowed variance explained')
                     # ylim=(-.025, .55))
 
         # numbers on top indicating current regime
         for p in range(regime_changes+1): 
             trans = transforms.blended_transform_factory(currax.transData, 
-                                                         currax.transAxes)
+                                                        currax.transAxes)
             regime_start = curr_t * p
-            text_x_loc = (regime_start + (curr_t)) / l # in data coords for ease
+            text_x_loc = (regime_start + (curr_t)-125) / l # in data coords for ease
             currax.text(text_x_loc, 1.02, '{}'.format(regime_order[p]+1), 
                         transform=trans, color=cmap(1-regime_order[p])) # 1- for annoying color thing
 
     # dotted line indicating regime change
     # signal
     for j, currax in enumerate(ax[row, :]):
-        for lineloc in [(curr_t*(i+1))/l for i in range(regime_changes+1)]:
+        for lineloc in [(curr_t*(i+1)-l1)/l for i in range(regime_changes+1)]:
             if j == 0:
                 lineloc *= l
             currax.axvline(lineloc, ls='--', color='grey', alpha=.6)
@@ -174,19 +217,76 @@ for row, curr_t in enumerate(t_latent):
 ax[0, 1].legend(loc='upper right')
 axtwin.legend()
 plt.suptitle('streaming SVD does not clearly identify spectral regime changes',
-             y=1.12)
-
+            y=1.12)
 # plt.savefig('stream_svd_problem.png', bbox_inches='tight')
 
-u, s, v = np.linalg.svd(xs, full_matrices=False)
-u = u[:, :k]
-basis = pro.U
-res1 = np.linalg.norm(basis - u @ u.T @ basis)
-res2 = np.linalg.norm(u - basis @ basis.T @ u)
+# u, s, v = np.linalg.svd(xs, full_matrices=False)
+# u = u[:, :k]
+# basis = pro.U
+# res1 = np.linalg.norm(basis - u @ u.T @ basis)
+# res2 = np.linalg.norm(u - basis @ basis.T @ u)
+# print(res1, res2)
 
-print(res1, res2)
 
-fig, ax = plt.subplots() 
-ax.plot(derivs.T)
+
+# plt.figure()
+# plt.imshow(np.abs(u - pro.U))
+# plt.colorbar()
+
+fig, ax = plt.subplots(1, 3, figsize=(15, 4))
+ax[0].plot(pro.Ss.T)
+labels = ['streaming SVD power shifts', 'proSVD power shifts']
+for i in range(2):
+    normdiffs = np.linalg.norm(np.diff(projs[i]**2, axis=1), axis=0)
+    ax[i+1].plot(normdiffs, label=labels[i])
+# ax[2].legend()
+
+titles = ['singular values over time', 
+        'changes in streaming SVD power:\n' + r'$ norm(diff((U^\top A)^2))$',
+        'changes in proSVD power:\n' + r'$ norm(diff((Q^\top A)^2))$']
+for i, currax in enumerate(ax):
+    for p in range(regime_changes+1): 
+        trans = transforms.blended_transform_factory(currax.transData, 
+                                                        currax.transAxes)
+        regime_start = curr_t * p
+        text_x_loc = (regime_start + (curr_t)-125) / l # in data coords for ease
+        currax.text(text_x_loc, 1.02, '{}'.format(regime_order[p]+1), 
+                    transform=trans, color=cmap(1-regime_order[p])) # 1- for annoying color thing
+
+    # dotted line indicating regime change
+    for lineloc in [(curr_t*(i+1)-l1)/l for i in range(regime_changes+1)]:
+        if j == 0:
+            lineloc *= l
+        currax.axvline(lineloc, ls='--', color='grey', alpha=.6)
+    currax.set_title(titles[i], y=1.08)
+
+
+titles = ['streaming SVD squared projections', 'proSVD squared projections']
+for b in range(2):
+    fig, ax = plt.subplots(3, 2, figsize=(10, 1.5*k))
+    plt.subplots_adjust(hspace=.3)
+    c = 0
+    for w in range(2):
+        for i, currax in enumerate(ax[:, w]):
+            currax.plot((projs[b][c, :]**2).T, c='C{}'.format(c))
+            for p in range(regime_changes+1): 
+                trans = transforms.blended_transform_factory(currax.transData, 
+                                                                currax.transAxes)
+                regime_start = curr_t * p
+                text_x_loc = (regime_start + (curr_t)-125) / l # in data coords for ease
+                currax.text(text_x_loc, 1.02, '{}'.format(regime_order[p]+1), 
+                            transform=trans, color=cmap(1-regime_order[p])) # 1- for annoying color thing
+
+            # dotted line indicating regime change
+            for lineloc in [(curr_t*(i+1)-l1)/l for i in range(regime_changes+1)]:
+                if j == 0:
+                    lineloc *= l
+                currax.axvline(lineloc, ls='--', color='grey', alpha=.6)
+            
+            c += 1
+    plt.suptitle(titles[b], y=.95)
+# plt.savefig('stream_svd_problem2.png', bbox_inches='tight')
+
+# %%
 
 # %%
--- proSVD.py
+++ proSVD.py
@@ -15,7 +15,7 @@ class proSVD:
     # history      int     - 0 indicates no history will be kept, 
     #                       >0 indicates how many bases/singular values to keep
     # track_prev   bool    - whether you want to keep the previous Q (incurs copying - better way to do this?)
-    def __init__(self, k, w_len=1, w_shift=None, decay_alpha=1, trueSVD=False, history=0, track_prev=True):
+    def __init__(self, k, w_len=1, w_shift=None, decay_alpha=1, trueSVD=False, history=0, track_prev=True, mode='pro'):
         self.k = k
         self.w_len = w_len
         self.w_shift = w_len if w_shift is None else w_shift # defauls to nonoverlapping chunks of w_len cols
@@ -24,6 +24,7 @@ class proSVD:
         self.history = history
         self.track_prev = track_prev
         self.proj_mean = np.zeros((k))  # for global mean of projected data (to get projected variance)
+        self.mode = mode
 
     
     def initialize(self, A_init, Q_init=None, B_init=None):
@@ -41,7 +42,6 @@ class proSVD:
         self.Q = Q[:, :self.k] if Q_init is None else Q_init
         self.B = B[:self.k, :l1] if B_init is None else B_init
 
-        # self.W = np.eye(l1) # TODO: figure out if we want W
         if self.trueSVD:
             U_init, S_init, _ = np.linalg.svd(A_init, full_matrices=False)
             self.U = U_init[:, :self.k]
@@ -50,11 +50,8 @@ class proSVD:
         if self.history:
             ## these may need to be some kind of circular buffer
             ## for now assuming self.history = A_full.shape[1] - l1 (if not 0)
-            self.Qs = np.zeros((n, self.k, self.history+1))
+            self.Qs = np.zeros((n, self.k, self.history))
             self.Qs[:, :, 0] = self.Q # init with first Q
-
-            # this might need to be different?
-            # self.Ws = np.zeros((self.k, self.w_len, self.history))
 
             # keeping true singular vectors/values
             if self.trueSVD:
@@ -71,37 +68,23 @@ class proSVD:
     # updates proSVD 
     # outputs: projections, variance explained, derivatives 
     def run(self, A, num_init, num_iters=None, ref_basis=None):
-        if num_iters is None: # do iters to go through once
+        # getting index of update times
+        if num_iters is None: # do enough iters to go through once
             num_iters = np.floor((A.shape[1] - num_init - self.w_len)/self.w_shift).astype('int')
         update_times = np.arange(num_init, num_iters*self.w_shift, self.w_len) # index of when updates happen (not including init)
         
-        # for svd and prosvd projections, variance explained
-        projs = [np.zeros((self.k, A.shape[1]-num_init)) for i in range(2)]  # subtract l1 - init proj
-        frac_vars = [np.zeros(projs[i].shape) for i in range(2)]
-        # derivatives
-        derivs = np.zeros((self.k, num_iters))
-
         # run proSVD online
         for i, t in enumerate(update_times): 
-            dat = A[:, t:t+self.w_len]
+            start, end = t, t+self.w_len
+            dat = A[:, start:end]
 
             # TODO: run should take user input pre/postupdate functions
             # they should be executed here
             self.preupdate()
             self._updateSVD(dat, ref_basis)
-            self.postupdate()
-
-            # TODO: move this to postupdate
-            # getting proj and variance explained
-            for j, basis in enumerate([self.U, self.Q]):
-                projs[j][:, t:t+self.w_len] = basis.T @ dat
-                curr_proj_vars = projs[j][:, :t].var(axis=1)[:, np.newaxis]
-                total_vars = A[:, num_init:t].var(axis=1)
-                frac_vars[j][:, t:t+self.w_len] = curr_proj_vars / total_vars.sum()
-            # proSVD basis derivatives
-            derivs[:, i] = np.linalg.norm(self.Q-self.Q_prev, axis=0)
-
-        return projs, frac_vars, derivs
+            self.postupdate(dat, start, end, i, t, num_init, A)
+            
+        return
         
     # internal func to do a single iter of basis update given some data A
     def _updateSVD(self, A, ref_basis=None):
@@ -121,52 +104,48 @@ class proSVD:
         tmp = np.zeros((B_perp.shape[0], self.B.shape[1]))
         tmp = np.concatenate((tmp, B_perp), axis=1)
         B_hat = np.concatenate((B_prev, tmp), axis=0)
-
-        # W_hat is I_l appended as block to W
-        # I_l = np.eye(l)
-        # right_block = np.zeros((self.W.shape[0], l))
-        # bottom_block = np.zeros((l, self.W.shape[1]))
-        # W_hat = np.block([[self.W, right_block], 
-        #                   [bottom_block, I_l]])
         
         ## Constructing orthogonal Gu and Gv from Tu and Tv
         # SVD of B_hat 
         U, diag, V = np.linalg.svd(B_hat, full_matrices=False)
 
-        # decaying (implements forgetting)
-        # diag = np.power(diag, self.decay_alpha)
+        # decaying singular values (implements forgetting)
         diag *= self.decay_alpha
         
-        # Orthgonal Procrustes singular basis for Q (getting Tu)
-        # solution for a 'reference' basis
-        if ref_basis is not None:
-            Mu = ref_basis.T @ Q_hat @ U[:, :self.k]
-        else: # solution for minimum change from previous basis
-            # Mu = self.Q.T @ Q_hat @ U[:, :self.k]
-            # faster getting Mu - just the first k rows of U1??
-            Mu = U[:self.k, :self.k]
-
-        U_tilda, _, V_tilda_T = np.linalg.svd(Mu, full_matrices=False)
-        Tu = U_tilda @ V_tilda_T
-
-        # Orthogonal Procrustes singular basis for W (getting Tv)
-        # TODO: W_j-1 is smaller than W_hat?
-        # truncate first L rows of W_hat
-        # Mv = self.W.T @ W_hat[l:, :] @ V[:, :self.k]
-        # U_tilda, _, V_tilda = np.linalg.svd(Mv, full_matrices=False)
-        # Tv = U_tilda @ V_tilda
-
-        # simpler way of getting Tv
-        V1 = (V.T)[:,0:self.k]
-        _, Tv = rq(V1) 
+        # getting Tu
+        if self.mode == 'pro':    # Orthogonal Procrustes solution
+            if ref_basis is not None:    # solution for a 'reference' basis
+                Mu = ref_basis.T @ Q_hat @ U[:, :self.k]
+            else:    # solution for minimum change from previous basis
+                Mu = U[:self.k, :self.k]    # same as Mu = self.Q.T @ Q_hat @ U[:, :self.k]
+                
+            U_tilda, _, V_tilda_T = np.linalg.svd(Mu, full_matrices=False)
+            Tu = U_tilda @ V_tilda_T
+            Gu_1 = U[:, :self.k] @ Tu.T
+        else:   
+            # Gu_1, Tu = rq(U[:, :self.k])  # Baker et al.
+            Gu_1 = U[:, :self.k] # Sequential KL 
+            Tu = Gu_1.T @ U[:, :self.k]
+            
+        # getting Tv
+        if True: # self.Tv_mode == 'baker':
+            Gv_1, Tv = rq(V[:, :self.k])
+        else:
+            Gv_1 = V[:, :self.k] # sequential KLT
+            Tv = Gv_1.T @ V[:, :self.k]
 
         ## UPDATING Q, B
-        Gu_1 = U[:, :self.k] @ Tu.T
-        # Gv_1 = V[:, :self.k] @ Tv.T
         self.Q = Q_hat @ Gu_1
         self.B = Tu @ np.diag(diag[:self.k]) @ Tv.T
-        # self.W = W_hat @ Gv_1
-
+        
+        # Getting true SVD basis
+        if self.mode != 'seq-kl':
+            if self.trueSVD:
+                U, S, V = np.linalg.svd(self.B)
+                self.U = self.Q @ U
+                self.S = S
+            
+        return
 
     # preupdate
     def preupdate(self):
@@ -176,50 +155,11 @@ class proSVD:
 
     # postupdate
     def postupdate(self):
-        # Getting true SVD basis
-        if self.trueSVD:
-            U, S, V = np.linalg.svd(self.B, full_matrices=False)
-            self.U = self.Q @ U
-            self.S = S
-            # self.Wt = self.W @ V
         # updating history
         if self.history:
             self.Qs[:, :, self.t] = self.Q
-            # self.Ws[:, :, self.t] = self.W
             if self.trueSVD:
                 self.Us[:, :, self.t] = self.U
                 self.Ss[:, self.t] = self.S
-                # self.Wts[:, :, self.t] = self.Wt
+        
         self.t += 1
-
-   # update the SVD with some given data (DEPRECATED)
-    # A should be in shape (n, t) (getting new colums of data)
-    def updateSVD(self, A, ref_basis=None, chunk_size=0):
-        n, s = A.shape
-        
-        if chunk_size == 0:  # process all of A (as one big chunk) and update basis once
-            num_iter = 1
-            l = s
-        else:  # process A with smaller chunks at a time, update basis multiple times
-            num_iter = int(np.ceil(s / chunk_size))  # iters to go through data once
-            l = chunk_size
-        
-        t = 0
-        for i in range(num_iter):
-            
-            A_plus = A[:, t:t+l]
-            t = t+l
-
-            # ------ Update ------ #
-            self._updateSVD(A_plus, ref_basis)
-            # -------------------- #
-
-            if self.history:
-                self.Qs[:, :, self.t] = self.Q
-                # self.Ws[:, :, self.t] = self.W
-                if self.trueSVD:
-                    self.Us[:, :, self.t] = self.U
-                    self.Ss[:, self.t] = self.S
-                    # self.Wts[:, :, self.t] = self.Wt
-
-            self.t += 1
